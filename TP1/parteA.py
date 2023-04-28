e25fea0189a3050d3afee388e007f086cab0e23a
Gino Bartolucci <ginobartolucci00@gmail.com>
2023-04-27 21:56:57 -0300

    Parte A-mSeparar funciones en una clase en archivo aparte, el loop de parte A

---
+++ TP1/parteA.py
@@ -0,0 +1,43 @@
+import numpy as np
+import funciones as f
+
+PROB_CROSSOVER = 0.75
+PROB_MUTACION = 0.05
+CICLOS = 200
+CANTIDAD_POBLACION = 10
+COEF = 1073741823 # 2^29
+
+
+rng = np.random.default_rng(12345) # Random Number Generator
+# El 12345 es la semilla, poner sin semilla para q en cada ejecución genere nuevos aleatorios.
+
+#Poblacion inicial
+poblacion_decimal = rng.integers(low=0, high=COEF, size=CANTIDAD_POBLACION)
+poblacion_binarios = f.Funciones.convertirPoblacionBin(poblacion_decimal)
+
+#Marco de datos inicial
+marcoDeDatos = f.Funciones.generarDataFrame(poblacion_decimal, poblacion_binarios, COEF)
+marcoDeDatos_stats = f.Funciones.generarEstadisticas(marcoDeDatos)
+print('CICLO: 0')
+print('\n TABLA DE DATOS: \n',marcoDeDatos)
+print('\n ESTADISTICAS: \n',marcoDeDatos_stats)
+for i in range(0, 1000):
+    # Seleccion
+    seleccion = f.Funciones.ruleta(marcoDeDatos, CANTIDAD_POBLACION)
+    print('\nRULETA: ')
+    print('\n'.join(map(str, seleccion)))
+    # Crossover
+    crossover = f.Funciones.crossover(seleccion, PROB_CROSSOVER)
+    print('\nCROSSOVER: ')
+    print('\n'.join(map(str, crossover)))
+    # Mutacion
+    #falta mutacion
+    # nueva poblacion
+    print('-------------------------------------------------------------------------')
+    print('\nCICLO: ', i+1)
+    marcoDeDatos = f.Funciones.generarDataFrame(f.Funciones.convertirPoblacionDecimal(crossover), crossover, COEF)
+    marcoDeDatos_stats = f.Funciones.generarEstadisticas(marcoDeDatos)
+    print('\n TABLA DE DATOS: \n',marcoDeDatos)
+    print('\n ESTADISTICAS: \n',marcoDeDatos_stats)
+    
+
