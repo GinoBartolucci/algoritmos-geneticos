2e939db392beb799a68954c15a8b624360630705
Francisco <franmendi.fm@gmail.com>
2023-11-12 23:47:32 -0300

    Refactored DNA and Crossover classes

--- TP3/AlgoritmoGenetico/Crossover.py
+++ TP3/AlgoritmoGenetico/Crossover.py
@@ -8,36 +8,41 @@ class Crossover:
         if random.random() < cromosoma1.getProbCrossover():
             # print("#### CROSSOVER ####")
             punto_corte = random.randint(0,len(cromosoma1.genes))
-            genes_hijo1 =  cromosoma1.genes[:punto_corte] + cromosoma2.genes[punto_corte:] 
-            genes_hijo2 =  cromosoma2.genes[:punto_corte] + cromosoma1.genes[punto_corte:] 
-            # print("Genes hijo 1 e hijo2 \n%s" % genes_hijo1)
-            # print(genes_hijo2)
+            hijo1 =  cromosoma1.genes[:punto_corte] + cromosoma2.genes[punto_corte:] 
+            hijo2 =  cromosoma2.genes[:punto_corte] + cromosoma1.genes[punto_corte:] 
+            # print("Genes hijo 1 e hijo2 \n%s" % hijo1)
+            # print(hijo2)
             # print("#### END ####")
 
-            return True,genes_hijo1,genes_hijo2
+            return True,hijo1,hijo2
         else:
             return False,cromosoma1.genes,cromosoma2.genes
         
-    @staticmethod
-    def ciclico(cromosoma1,cromosoma2) -> list:
-        def inicializar_genes_hijos(cromosoma1,cromosoma2):
-            return cromosoma1.genes.copy(), cromosoma2.genes.copy()
-        
-        def copiar_genes(cromosoma,genes_hijo):
-            for i in range(len(cromosoma.genes)):
-                if genes_hijo[i] == -1:
-                    genes_hijo[i] = cromosoma.genes[i]
-            return genes_hijo
-        
-        genes_hijo1, genes_hijo2 = inicializar_genes_hijos(cromosoma1,cromosoma2)
-        while -1 in genes_hijo1 or -1 in genes_hijo2:
-            punto_inicio = random.randint(0, len(cromosoma1.genes) - 1)
-            punto_actual = punto_inicio
-            while True:
-                genes_hijo1[punto_actual], genes_hijo2[punto_actual] = genes_hijo2[punto_actual], genes_hijo1[punto_actual]
-                punto_actual = cromosoma1.genes.index(cromosoma2.genes[punto_actual])
-                if punto_actual == punto_inicio:
-                    break
- 
-        return True, genes_hijo1, genes_hijo2
-        
+    
+
+    def ciclico(cromosoma1, cromosoma2): 
+        hijo1, hijo2 = cromosoma1.genes.copy(), cromosoma2.genes.copy()
+
+        punto_inicio = random.randint(0, len(cromosoma1.genes) - 1)
+        punto_actual = punto_inicio
+        genes_visitados = set()   
+
+        while True:
+            # Intercambiar genes
+            hijo1[punto_actual], hijo2[punto_actual] = hijo2[punto_actual], hijo1[punto_actual]
+            genes_visitados.add(punto_actual)
+
+            # Encontrar el próximo índice en el ciclo
+            punto_actual = cromosoma1.genes.index(cromosoma2.genes[punto_actual])
+
+            se_completo_ciclo = punto_actual == punto_inicio
+            if se_completo_ciclo:
+                break
+
+        # Llenar los genes que no están en el ciclo directamente de los padres
+        for ciudad in range(len(cromosoma1.genes)):
+            if ciudad not in genes_visitados:
+                hijo1[ciudad] = cromosoma1.genes[ciudad]
+                hijo2[ciudad] = cromosoma2.genes[ciudad]
+
+        return True, hijo1, hijo2
--- TP3/AlgoritmoGenetico/DNA.py
+++ TP3/AlgoritmoGenetico/DNA.py
@@ -2,7 +2,7 @@ import random
 # from Seleccion import Seleccion
 from Cromosoma import Cromosoma
 
-# la clase poblacion, tendremos tantos DNA como ciclos
+
 class DNA: 
 
     @staticmethod
@@ -19,13 +19,6 @@ class DNA:
         return poblacion
  
 
-    @staticmethod
-    def generar_cromosoma(cantidad_genes):
-        genes = [] 
-        for iteracion in range(cantidad_genes):
-            gen = random.randint(0,1) 
-            genes.append(gen) 
-        return Cromosoma(genes=genes)
 
     @staticmethod
     def generar_poblacion_numeros(cantidad_cromosomas:int):
@@ -43,24 +36,19 @@ class DNA:
 
     def __init__(self,poblacion:list=None,ciclos:int=20):
         self.poblacion = poblacion
-        self.fitness = {"Suma":0,"Promedio":0,"Maximo":0}
-        self.objetivo = {"Suma":0,"Promedio":0,"Maximo":0}
         self.ciclos = ciclos 
  
     def calcular_objetivo(self,funcion):
-        """
-        Mi idea aca es ir sumando la distancia entre las distintas ciudades
-        tener en cuenta que debo tomar entre dos ciudades la distancia
-        """
-        # print("self.poblacion",len(self.poblacion))
         for cromosoma in self.poblacion:
             distancia_total = 0
             distancia_total += funcion(cromosoma.genes[-1],cromosoma.genes[0])
+            
             for posicion in range(0,len(cromosoma.genes)-1):
                 ciudad_A,ciudad_B = cromosoma.genes[posicion],cromosoma.genes[posicion+1]
                 distancia_ciudades = funcion(ciudad_A,ciudad_B) 
                 distancia_total += distancia_ciudades
             index = self.poblacion.index(cromosoma)
+            
             self.poblacion[index].objetivo = distancia_total
  
         objetivos = []
@@ -71,27 +59,10 @@ class DNA:
         return objetivos
 
     def calcular_fitness(self,funcion):
-        # total = sum([cromosoma.objetivo for cromosoma in self.poblacion])
         for cromosoma in self.poblacion:
-            # print(cromosoma.objetivo)
             fitness = funcion(cromosoma.objetivo)
             cromosoma.fitness = fitness 
-
-        return self.fitness
  
-
-    def get_info_poblacion(self):
-        info = []
-        info_decimal = []
-        info_fitness = []
-        info_objetivo = []
-
-        for cromosoma in self.poblacion:
- 
-            info_fitness.append(cromosoma.fitness)
-            info_objetivo.append(cromosoma.objetivo)
-
-        return info,info_decimal,info_fitness,info_objetivo
 
 
     @staticmethod
